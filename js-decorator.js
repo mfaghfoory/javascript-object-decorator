41693478e7a6c4173da5ad0dfeace72030f5154d
User0 <User0@DESKTOP-IGLI7V8>
2019-05-19 11:58:25 +0430

    init

---
+++ js-decorator.js
@@ -0,0 +1,36 @@
+
+/**
+ * It takes an object and decorates over all its functions
+ * @param obj An object that you want to decorate all its methods
+ * @param beforeCall Function that calling 'before' each obj's functions
+ * @param afterCall Function that calling 'after' each obj's functions
+ * @param onException Function that calling when each obj's functions raise any error
+ */
+function decorate(obj, beforeCall, afterCall, onException) {
+  if (beforeCall && typeof beforeCall != "function")
+    throw "beforeCall is not a function";
+  if (afterCall && typeof afterCall != "function")
+    throw "afterCall is not a function";
+  if (onException && typeof onException != "function")
+    throw "onException is not a function";
+  for (let x in obj) {
+    if (obj.hasOwnProperty(x)) {
+      if (typeof obj[x] == "function") {
+        const innerMethod = obj[x].bind(obj);
+        obj[x] = function(...args) {
+          let result = null;
+          try {
+            beforeCall && beforeCall({ method: x, args });
+            result = innerMethod(args);
+          } catch (error) {
+            onException && onException({ error });
+          } finally {
+            afterCall && afterCall({ method: x, args, result });
+          }
+        };
+      }
+    }
+  }
+  return obj;
+}
+module.exports = decorate;
---
+++ spec/decorator-spec.js
@@ -0,0 +1,43 @@
+const decorate = require('../js-decorator')
+
+describe("decorator-basics", function() {
+  it("should decorate method", function() {
+    let obj = {
+      name: "foo",
+      print() {
+        return this.name;
+      }
+    };
+    let afterCall = null;
+    obj = decorate(obj, (res) => {}, (res) => { afterCall = res; });
+    obj.print();
+    expect(obj.name).toEqual(afterCall.result);
+  });
+
+  it("should return args", function() {
+    let obj = {
+      name: "foo",
+      print(preText) {
+        return preText + ' ' + this.name;
+      }
+    };
+    let beforeCall = null;
+    let afterCall = null;
+    obj = decorate(obj, (res) => { beforeCall = res; }, (res) => { afterCall = res; });
+    obj.print('hi');
+    expect('hi').toEqual(beforeCall.args[0]);
+  });
+
+  it("should grab error", function() {
+    let obj = {
+      name: "foo",
+      print(preText) {
+        throw 'error raised!'
+      }
+    };
+    let exceptionResult = null;
+    obj = decorate(obj, (res) => { }, (res) => { }, (err) => {exceptionResult = err.error;});
+    obj.print('hi');
+    expect(exceptionResult.toString()).toEqual('error raised!');
+  });
+});
